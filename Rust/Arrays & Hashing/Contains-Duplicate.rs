32e91acea7f69dee92df3d6d78109ab3dc943323
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-11-21 17:37:29 +0300

    Update

---
+++ Rust/Arrays & Hashing/Contains-Duplicate.rs
@@ -0,0 +1,20 @@
+use std::collections::HashSet;
+
+struct Solution {}
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut set: HashSet<i32> = HashSet::new();
+
+        for num in nums {
+            if set.contains(&num) {
+                return true;
+            }
+            set.insert(num);
+        }
+
+        false
+    }
+}
+
+fn main() {}
---
+++ Rust/Arrays & Hashing/Top-K-Frequent-Elements.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+struct Solution {}
+
+impl Solution {
+    pub fn top_k_frequents(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut map: HashMap<i32, i32> = HashMap::new();
+
+        for num in nums {
+            *map.entry(num).or_insert(0) += 1;
+        }
+
+        let mut x: Vec<(&i32, &i32)> = map.iter().collect();
+        x.sort_by(|&a, &b| (b.1).cmp(&a.1));
+        dbg!(&x);
+        let mut result = vec![];
+
+        for i in 0..k {
+            result.push(*x[i as usize].0);
+        }
+
+        result
+    }
+}
+
+fn main() {
+    Solution::top_k_frequents(vec![4, 1, -1, 2, -1, 2, 3], 2);
+}
