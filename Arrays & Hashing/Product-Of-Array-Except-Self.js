e886dded03770699f2a5c77b3888ac585934bc4c
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-11-21 14:38:13 +0300

    Update Product-Of-Array-Except-Self.js

--- Arrays & Hashing/Product-Of-Array-Except-Self.js
+++ Arrays & Hashing/Product-Of-Array-Except-Self.js
@@ -19,4 +19,28 @@ const productExceptSelf = (nums) => {
   return prefix.map((element, index) => element * postfix[index]);
 };
 
-productExceptSelf([1, 2, 3, 4]);
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * Time O(N) | Space O(1)
+ */
+
+const productExceptSelf1 = (nums) => {
+  let result = new Array(nums.length).fill(1);
+
+  let prefix = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let postfix = 1;
+
+  for (let i = result.length - 1; i >= 0; i--) {
+    result[i] *= postfix;
+    postfix *= nums[i];
+  }
+
+  return result;
+};
