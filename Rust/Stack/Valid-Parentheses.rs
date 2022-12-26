26af17f45ad1e8c57683bea659e587fb21715a4c
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-12-26 17:47:27 +0300

    update

---
+++ Rust/Stack/Valid-Parentheses.rs
@@ -0,0 +1,20 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+
+        for i in s.chars() {
+            match i {
+                '{' => stack.push('}'),
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                '}' | ')' | ']' if Some(i) != stack.pop() => return false,
+                _ => (),
+            }
+        }
+
+        stack.is_empty()
+    }
+}
+fn main() {}
